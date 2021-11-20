7df761fc7b49adc80f191b83bbed2aef3dfef3f7
“Nicole <nicolvn1@uci.edu>
2021-11-20 14:37:41 -0800

    commit change

--- canvas.js
+++ canvas.js
@@ -1,47 +1,62 @@
 window.onload = function() {
 
 var canvas = new fabric.Canvas('c', {isDrawingMode:true});
-canvas.backgroundColor = 'rgb(170,210,250)'
+canvas.backgroundColor = 'rgb(255, 255, 255)'
+
 // create a rectangle object
 var rect = new fabric.Rect({
-  left: 100,
-  top: 100,
-  fill: 'yellow',
-  width: 100,
-  height: 100
-});
+    left: 100,
+    top: 100,
+    fill: 'yellow',
+    width: 100,
+    height: 100
+  });
 
 // "add" rectangle onto canvas
-// canvas.add(rect);
+canvas.add(rect);
 
-// var text = window.prompt("Enter text: ");
-
-//make a function? --> when called, a sticky note is created
+// make a function? --> when called, a sticky note is created
 var text = new fabric.Textbox("Insert text", {
   fontSize: 14,
   width: 100,
   cursorColor: "black",
-  top: 100,
-  left: 100,
+  top: 50,
+  left: 50,
   editable: true
 });
 
 canvas.add(rect)
 canvas.add(text)
 
-var group = new fabric.Group([ rect, text ], {
-    left: 150,
-    top: 100,
-  });
+// var group = new fabric.Group([ rect, text ], {
+//     left: 150,
+//     top: 100,
+//   });
   
-  canvas.add(group);
+//   canvas.add(group);
+rect.set('selectable', true)
+text.set('selectable', true)
+  
+
+
+
+
+
+
+
+
 
 // send drawing to backend
 
-canvas.on('path:created',)
+// send user drawing to back end
+
+canvas.on('path:created',function(event){
+  console.log()
 
 
-//   rect.set('selectable', true)
+})
+
+
 
 
 
